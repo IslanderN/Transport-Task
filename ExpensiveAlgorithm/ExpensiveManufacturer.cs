b026a63e4f92827a7b397b4f4e771ffc87d06097
Volodymyr <vova.lev.16@gmail.com>
2019-05-19 01:08:17 +0300

    Expensive Algorithm

---
+++ ExpensiveAlgorithm/ExpensiveManufacturer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonClasses;
+
+namespace ExpensiveAlgorithm
+{
+   public class ExpensiveManufacturer:Manufacture
+    {
+        public int CapacityFree { get; set; }
+
+        public ExpensiveManufacturer():base(){}
+        public ExpensiveManufacturer(Manufacture manufacture) : base() {
+            this.IsOrganisated = manufacture.IsOrganisated;
+            this.OrganisationCost = manufacture.OrganisationCost;
+            this.ProbabilityOfOrganisate = manufacture.ProbabilityOfOrganisate;
+            this.ProductionCapacity = manufacture.ProductionCapacity;
+            this.CapacityFree = manufacture.ProductionCapacity;
+            this.ClientsDeliveryCost = manufacture.ClientsDeliveryCost;
+        }
+
+
+
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using CommonClasses;
 using GeneticAlgorithm;
+using ExpensiveAlgorithm;
 
 namespace Runner
 {
@@ -13,9 +14,45 @@ namespace Runner
     {
         static void Main(string[] args)
         {
-            
+            Expensive();
 
             Console.ReadKey();
+        }
+
+        static void Expensive()
+        {
+            FileInput f = new FileInput("input.txt");
+            List<Client> clients;
+            List<Manufacture> manufactures;
+            List<ExpensiveManufacturer> expensiveManufacturers;
+            int clientNeeds;
+            int manufacturerNeeds = 0;
+
+            f.ReadFromFile(out clients, out manufactures);
+
+            Expensive algorithm = new Expensive();
+            
+            expensiveManufacturers = algorithm.SetManufacturePriority(manufactures);
+
+            clientNeeds = clients.Sum(a => a.Needs);
+
+            int i = 0;
+            while (manufacturerNeeds <= clientNeeds)
+            {
+                manufacturerNeeds += expensiveManufacturers[0].ProductionCapacity;
+                expensiveManufacturers[0].IsOrganisated = 1;
+                i++;
+            }
+
+            int sum = 0;
+            foreach(var el in expensiveManufacturers)
+            {
+                Console.WriteLine(el.ProductionCapacity.ToString());
+                sum += el.ProductionCapacity;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(clientNeeds);
+
         }
 
         static void Genetic()
